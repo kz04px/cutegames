929a878b459ad441ac0c89c7ba5ab9aed66d0ff4
kz04px <6536046+kz04px@users.noreply.github.com>
2024-03-10 17:31:51 +0000

    Test strong vs weak player game results

--- tests/games/ataxx.cpp
+++ tests/games/ataxx.cpp
@@ -83,6 +83,98 @@ class TestEngine final : public Engine {
     libataxx::Position m_pos;
 };
 
+class StrongTestEngine final : public Engine {
+   public:
+    virtual ~StrongTestEngine() {
+    }
+
+    [[nodiscard]] virtual auto is_running() -> bool override {
+        return true;
+    }
+
+    virtual auto init() -> void override {
+    }
+
+    virtual auto is_ready() -> void override {
+    }
+
+    virtual auto newgame() -> void override {
+        m_pos.set_fen("startpos");
+    }
+
+    virtual auto quit() -> void override {
+    }
+
+    virtual auto stop() -> void override {
+    }
+
+    virtual auto position(const std::string &start_fen, const std::vector<std::string> &move_history) -> void override {
+        m_pos.set_fen(start_fen);
+        for (const auto &movestr : move_history) {
+            const auto move = libataxx::Move::from_uai(movestr);
+            m_pos.makemove(move);
+        }
+    }
+
+    virtual auto set_option(const std::string &, const std::string &) -> void override {
+    }
+
+    [[nodiscard]] virtual auto go(const SearchSettings &) -> std::string override {
+        num_go_received++;
+        const auto moves = m_pos.legal_moves();
+        std::stringstream ss;
+
+        auto best_score = -1;
+        auto best_move = libataxx::Move();
+        for (const auto &move : moves) {
+            const auto bb = libataxx::Bitboard(move.to());
+            const auto num_captures = (bb.singles() & m_pos.get_them()).count();
+            const auto score = 2 * num_captures + move.is_single();
+
+            if (num_captures == m_pos.get_them().count()) {
+                best_move = move;
+                break;
+            }
+
+            if (score > best_score) {
+                best_score = score;
+                best_move = move;
+            }
+        }
+
+        ss << best_move;
+        return ss.str();
+    }
+
+    [[nodiscard]] virtual auto query_p1turn() -> bool override {
+        return m_pos.get_turn() == libataxx::Side::Black;
+    }
+
+    [[nodiscard]] virtual auto query_gameover() -> bool override {
+        return m_pos.is_gameover();
+    }
+
+    [[nodiscard]] virtual auto query_result() -> std::string override {
+        switch (m_pos.get_result()) {
+            case libataxx::Result::BlackWin:
+                return "p1win";
+            case libataxx::Result::WhiteWin:
+                return "p2win";
+            case libataxx::Result::Draw:
+                return "draw";
+            case libataxx::Result::None:
+                return "none";
+            default:
+                throw std::runtime_error("Invalid game result");
+        }
+    }
+
+    int num_go_received = 0;
+
+   private:
+    libataxx::Position m_pos;
+};
+
 [[nodiscard]] static auto get_final(const std::shared_ptr<Game> game) -> libataxx::Position {
     auto pos = libataxx::Position(game->start_fen());
     for (const auto &movestr : game->move_history()) {
@@ -329,4 +421,107 @@ TEST_CASE("Ataxx - Draw") {
             REQUIRE(std::abs(engine1->num_go_received - engine2->num_go_received) <= 1);
         }
     }
+}
+
+TEST_CASE("Ataxx - Strong vs Weak") {
+    const std::array fens = {
+        // Tournament openings
+        "startpos",
+        "x5o/7/2-1-2/7/2-1-2/7/o5x x 0 1",
+        "x5o/7/3-3/2-1-2/3-3/7/o5x x 0 1",
+        "x2-2o/3-3/2---2/7/2---2/3-3/o2-2x x 0 1",
+        "x2-2o/3-3/7/--3--/7/3-3/o2-2x x 0 1",
+        "x1-3o/2-4/2-1---/7/---1-2/4-2/o3-1x x 0 1",
+        "x5o/7/2-1-2/3-3/2-1-2/7/o5x x 0 1",
+        "x5o/7/3-3/2---2/3-3/7/o5x x 0 1",
+        "x5o/2-1-2/1-3-1/7/1-3-1/2-1-2/o5x x 0 1",
+        "x5o/1-3-1/2-1-2/7/2-1-2/1-3-1/o5x x 0 1",
+        // Unusual openings
+        "x-1-1-o/-1-1-1-/1-1-1-1/-1-1-1-/1-1-1-1/-1-1-1-/o-1-1-x x 0 1",
+        "x-1-1-o/1-1-1-1/1-1-1-1/1-1-1-1/1-1-1-1/1-1-1-1/o-1-1-x x 0 1",
+        "x1-1-1o/2-1-2/-------/2-1-2/-------/2-1-2/o1-1-1x x 0 1",
+        "x5o/1-----1/1-3-1/1-1-1-1/1-3-1/1-----1/o5x x 0 1",
+        "x-1-1-o/1-1-1-1/-1-1-1-/-1-1-1-/-1-1-1-/1-1-1-1/o-1-1-x x 0 1",
+        "x5o/1--1--1/1--1--1/7/1--1--1/1--1--1/o5x x 0 1",
+        "x-3-o/1-1-1-1/1-1-1-1/3-3/1-1-1-1/1-1-1-1/o-3-x x 0 1",
+        "x2-2o/3-3/3-3/-------/3-3/3-3/o2-2x x 0 1",
+        "x2-2o/2-1-2/1-3-1/-2-2-/1-3-1/2-1-2/o2-2x x 0 1",
+        // Some ply 3 fair
+        "x5o/7/7/7/7/7/oo2xxx o 0 2",
+        "x5o/7/7/7/o2x3/7/6x o 2 2",
+        "x6/4o2/7/7/4x2/7/o4x1 o 2 2",
+        "x5o/7/7/7/7/o3x2/o3x2 o 0 2",
+        "x3o2/7/7/7/7/5x1/o3x2 o 0 2",
+        "x5o/7/7/7/1o3x1/4x2/7 o 0 2",
+        "x5o/7/7/7/o3x2/5x1/7 o 0 2",
+    };
+
+    const auto game_type = GameType::Ataxx;
+    const auto timecontrol = SearchSettings{};
+    const auto adjudication = AdjudicationSettings{};
+    const auto protocol = ProtocolSettings{};
+    auto engine1 = std::make_shared<StrongTestEngine>();
+    auto engine2 = std::make_shared<TestEngine>();
+    auto num_p1_wins = 0;
+    auto num_p2_wins = 0;
+    auto num_engine1_wins = 0;
+    auto num_engine2_wins = 0;
+    auto num_draws = 0;
+
+    for (const auto &fen : fens) {
+        if (std::string(fen) != "startpos") {
+            REQUIRE(libataxx::Position(fen).get_fen() == fen);
+        }
+
+        engine1->num_go_received = 0;
+        engine2->num_go_received = 0;
+
+        for (const auto is_engine1_p1 : {true, false}) {
+            const auto gg = [&]() {
+                if (is_engine1_p1) {
+                    return play_game(game_type, timecontrol, adjudication, protocol, fen, engine1, engine2);
+                } else {
+                    return play_game(game_type, timecontrol, adjudication, protocol, fen, engine2, engine1);
+                }
+            }();
+            const auto pos = get_final(gg.game);
+
+            REQUIRE(gg.reason == AdjudicationReason::None);
+            REQUIRE(pos.is_gameover());
+            REQUIRE(gg.game->start_fen() == fen);
+            REQUIRE(std::abs(engine1->num_go_received - engine2->num_go_received) <= 1);
+
+            // Game result
+            switch (gg.result) {
+                case GameResult::Player1Win:
+                    REQUIRE_EQ(pos.get_result(), libataxx::Result::BlackWin);
+                    num_p1_wins++;
+                    num_engine1_wins += is_engine1_p1;
+                    num_engine2_wins += !is_engine1_p1;
+                    break;
+                case GameResult::Player2Win:
+                    REQUIRE_EQ(pos.get_result(), libataxx::Result::WhiteWin);
+                    num_p2_wins++;
+                    num_engine1_wins += !is_engine1_p1;
+                    num_engine2_wins += is_engine1_p1;
+                    break;
+                case GameResult::Draw:
+                    REQUIRE_EQ(pos.get_result(), libataxx::Result::Draw);
+                    num_draws++;
+                    break;
+                case GameResult::None:
+                    REQUIRE_EQ(pos.get_result(), libataxx::Result::None);
+                    FAIL("No game result");
+                    break;
+                default:
+                    FAIL("Unknown game result");
+                    break;
+            }
+        }
+    }
+
+    const auto games_played = 2 * fens.size();
+    REQUIRE(num_p1_wins + num_p2_wins + num_draws == games_played);
+    REQUIRE(num_engine1_wins > num_engine2_wins);
+    REQUIRE(num_engine1_wins >= static_cast<int>(0.9f * games_played));
 }
